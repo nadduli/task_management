f07ec1449486ab56b9929d5b07d8ee1218a52aab
nadduli <naddulidaniel94@gmail.com>
2024-11-03 02:38:02 +0300

    fix: bug in refreshtoken bearer

--- api/v1/auth/routes.py
+++ api/v1/auth/routes.py
@@ -7,7 +7,7 @@ from api.db.database import get_session
 from .schema import UserCreate, UserModel, LoginModel
 from .service import UserService
 from .utils import create_access_token, decode_access_token, verify_password
-from datetime import timedelta
+from datetime import timedelta, datetime
 from fastapi.responses import JSONResponse
 from .dependencies import RefreshTokenBearer
 
@@ -71,6 +71,12 @@ async def login_users(
 
 
 @auth_router.get("/refresh_token")
-async def get_new_access_token(token_details: dict = Depends(RefreshTokenBearer)):
+async def get_new_access_token(token_details: dict = Depends(RefreshTokenBearer())):
     """Create New Access Token"""
-    return {}
+    expiry_timestamp = token_details["exp"]
+    if datetime.fromtimestamp(expiry_timestamp) > datetime.now():
+        new_access_token = create_access_token(user_data=token_details["user"])
+
+        return JSONResponse(content={"access_token": new_access_token})
+
+    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid or Expired Token")
