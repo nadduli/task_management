b7ad715a8ce8ab1c34f08e503bb3ce1b472215b7
nadduli <naddulidaniel94@gmail.com>
2024-11-03 02:45:16 +0300

    added .env.example file

--- api/v1/auth/routes.py
+++ api/v1/auth/routes.py
@@ -79,4 +79,6 @@ async def get_new_access_token(token_details: dict = Depends(RefreshTokenBearer(
 
         return JSONResponse(content={"access_token": new_access_token})
 
-    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid or Expired Token")
+    raise HTTPException(
+        status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid or Expired Token"
+    )
