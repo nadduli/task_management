83aadeec7a6fcf08e906b8ccef52348fb09ea051
nadduli <naddulidaniel94@gmail.com>
2024-11-03 03:33:12 +0300

    cleanup

--- api/db/redis.py
+++ api/db/redis.py
@@ -2,3 +2,16 @@
 
 """REDIS Client"""
 
+import aioredis
+
+from api.core.config import Config
+
+token_blocklist = aioredis.StrictRedis(
+    host=Config.REDIS_HOST,
+    port=Config.REDIS_PORT,
+    db=0
+)
+
+async def add_jti_to_block_list(jti: str) -> None:
+    """Add token jti string to blacklist"""
+    await token_blocklist
