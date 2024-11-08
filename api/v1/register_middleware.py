7bfbb04a32516708c6546b39837cb8a41b511c87
nadduli <naddulidaniel94@gmail.com>
2024-11-08 16:45:15 +0300

    refactor: middleware

--- api/v1/register_middleware.py
+++ api/v1/register_middleware.py
@@ -3,6 +3,8 @@
 
 from fastapi import FastAPI
 from fastapi.requests import Request
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
 from time import time
 import logging
 
@@ -24,3 +26,13 @@ def register_middleware(app: FastAPI):
         message = f"{request.client.host}:{request.client.port} - {request.method} - {request.url} - {response.status_code} - completed in {process_time}s"
         print(message)
         return response
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+    app.add_middleware(TrustedHostMiddleware, allowed_hosts=["localhost", "127.0.0.1"])
