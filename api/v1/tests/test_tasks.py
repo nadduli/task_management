d3f30e36fee4259e687345ad65cf957e283efbf2
nadduli <naddulidaniel94@gmail.com>
2024-11-14 19:55:41 +0300

    removed cached vscode files

--- api/v1/tests/test_tasks.py
+++ api/v1/tests/test_tasks.py
@@ -11,3 +11,21 @@ def test_get_tasks(test_client, fake_task_service, fake_session):
 
     assert fake_task_service.get_tasks_called_once()
     assert fake_task_service.get_tasks_called_once_with(fake_session)
+
+def test_create_task(test_client, fake_task_service, fake_session):
+    """Test create tasks"""
+
+    task_data = {
+        "title": "test title",
+        "description": "test description",
+        "due_date": "2023-01-01",
+        "status": "todo",
+        "priority": "high",
+        "assigned_to": "naddulidaniel@gmail.com ",
+        "tags": ["test", "tag"],
+    }
+
+    response = test_client.post(url=f"{tasks_prefix}", json=task_data)
+
+    assert fake_task_service.create_task_called_once()
+    assert fake_task_service.create_task_called_once_with(task_data, fake_session)
